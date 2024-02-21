3d09183ababfa0912025f0990b93e7e894d03bf7
Valeriy <valeriy.elykomov@gmail.com>
2024-02-21 17:52:35 +0500

    Create CmdExtensions

---
+++ CryptoPro.Adapter/Extensions/CmdExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace CryptoPro.Adapter.CryptCP.Extensions
+{
+    internal static class CmdExtensions
+    {
+        public static string RunProcessCMD(this string cmd)
+        {
+            var processInfo = new ProcessStartInfo()
+            {
+                UseShellExecute = false,
+                FileName = "cmd.exe",
+                Arguments = "/c " + cmd,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            var process = new Process
+            {
+                StartInfo = processInfo
+            };
+
+            process.Start();
+
+            var log = string.Empty;
+
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var output = process.StandardOutput.ReadToEnd();
+                log += output;
+            }
+
+            process.WaitForExit();
+            process.Kill();
+
+            return log;
+        }
+    }
+}
