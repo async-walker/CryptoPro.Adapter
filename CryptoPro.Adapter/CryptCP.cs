c2ae9d379bd1be05a2124892632cca6d17bdebdb
Valeriy <valeriy.elykomov@gmail.com>
2024-02-21 17:52:06 +0500

    Update CryptCP

--- CryptoPro.Adapter/CryptCP.cs
+++ CryptoPro.Adapter/CryptCP.cs
@@ -1,7 +1,51 @@
-﻿namespace CryptoPro.Adapter
+﻿using CryptoPro.Adapter.CryptCP.Extensions;
+
+namespace CryptoPro.Adapter.CryptCP
 {
-    public class CryptCP
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CryptCP : ICryptCP
     {
-        
+        private readonly CryptCpConfiguration _config;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        public CryptCP(CryptCpConfiguration config)
+        {
+            _config = config;
+        }
+
+        /// <inheritdoc/>
+        public Task DeleteSiganture()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task Encrypt()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task<string> SignDataCreateMessage(
+            CriteriasSearchCertificate csc, 
+            string directoryToSave, 
+            string sourceFilePath)
+        {
+            var criterias = csc.ExecuteParams();
+
+            var cmd = $@"{_config.PathExe} -sign -dir {directoryToSave} {criterias} {sourceFilePath}";
+            cmd.RunProcessCMD();
+
+            var createdSigFilePath = string.Join(
+                separator: ".", 
+                value: [Path.Combine(directoryToSave, sourceFilePath), "sig"]);
+
+            return Task.FromResult(createdSigFilePath);
+        }
     }
 }
---
+++ CryptoPro.Adapter/ICryptCP.cs
@@ -0,0 +1,30 @@
+﻿namespace CryptoPro.Adapter.CryptCP
+{
+    /// <summary>
+    /// Интерфейс взаимодействия с утилитой CryptCP
+    /// </summary>
+    public interface ICryptCP
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task Encrypt();
+        /// <summary>
+        /// Подпись данных и создание сообщения (-sign)
+        /// </summary>
+        /// <param name="criterias"></param>
+        /// <param name="directoryToSave"></param>
+        /// <param name="sourceFilePath"></param>
+        /// <returns>Путь к созданному подписанному сообщению</returns>
+        Task<string> SignDataCreateMessage(
+            CriteriasSearchCertificate criterias, 
+            string directoryToSave, 
+            string sourceFilePath);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task DeleteSiganture();
+    }
+}
