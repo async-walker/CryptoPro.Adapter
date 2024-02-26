555c779fa9efee42a35a3c154a0ed330de56d627
Valeriy <valeriy.elykomov@gmail.com>
2024-02-26 17:33:18 +0500

    Update CryptCP

--- CryptoPro.Adapter/CryptCP.cs
+++ CryptoPro.Adapter/CryptCP.cs
@@ -3,16 +3,16 @@
 namespace CryptoPro.Adapter.CryptCP
 {
     /// <summary>
-    /// 
+    /// Класс взаимодействия с утилитой командной строки CryptCP
     /// </summary>
     public class CryptCP : ICryptCP
     {
         private readonly CryptCpConfiguration _config;
 
         /// <summary>
-        /// 
+        /// Инициализация экземпляра <see cref="CryptCP"/>
         /// </summary>
-        /// <param name="config"></param>
+        /// <param name="config">Конфигурация для настройки работы с утилитой</param>
         public CryptCP(CryptCpConfiguration config)
         {
             _config = config;
@@ -33,19 +33,27 @@ namespace CryptoPro.Adapter.CryptCP
         /// <inheritdoc/>
         public Task<string> SignDataCreateMessage(
             CriteriasSearchCertificate csc, 
+            bool der,
             string directoryToSave, 
             string sourceFilePath)
         {
             var criterias = csc.ExecuteParams();
 
-            var cmd = $@"{_config.PathExe} -sign -dir {directoryToSave} {criterias} {sourceFilePath}";
+            var cmd = $@"{_config.PathExe} -sign -dir {directoryToSave} {criterias} {UseDer(der)} {sourceFilePath}";
             cmd.RunProcessCMD();
-
+            
             var createdSigFilePath = string.Join(
                 separator: ".", 
                 value: [Path.Combine(directoryToSave, sourceFilePath), "sig"]);
 
             return Task.FromResult(createdSigFilePath);
+        }
+
+        static string UseDer(bool der)
+        {
+            if (der)
+                return "-der";
+            else return string.Empty;
         }
     }
 }
--- CryptoPro.Adapter/ICryptCP.cs
+++ CryptoPro.Adapter/ICryptCP.cs
@@ -6,23 +6,25 @@
     public interface ICryptCP
     {
         /// <summary>
-        /// 
+        /// Шифрование данных (-decr)
         /// </summary>
         /// <returns></returns>
         Task Encrypt();
         /// <summary>
         /// Подпись данных и создание сообщения (-sign)
         /// </summary>
-        /// <param name="criterias"></param>
-        /// <param name="directoryToSave"></param>
-        /// <param name="sourceFilePath"></param>
+        /// <param name="criterias">Критерии поиска сертификатов</param>
+        /// <param name="der">Использовать формат DER вместо BASE64</param>
+        /// <param name="directoryToSave">Директория, куда следует сохранить подписанное сообщение</param>
+        /// <param name="sourceFilePath">Путь к исходному файлу</param>
         /// <returns>Путь к созданному подписанному сообщению</returns>
         Task<string> SignDataCreateMessage(
             CriteriasSearchCertificate criterias, 
+            bool der,
             string directoryToSave, 
             string sourceFilePath);
         /// <summary>
-        /// 
+        /// Удаление подписи с сообщения (-delsign)
         /// </summary>
         /// <returns></returns>
         Task DeleteSiganture();
