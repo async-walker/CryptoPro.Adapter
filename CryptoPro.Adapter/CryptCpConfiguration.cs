89bae8031dff7cbc6911cb5d8a9b8e98ef5d23db
Valeriy <valeriy.elykomov@gmail.com>
2024-02-26 17:31:52 +0500

    Update CryptCpConfiguration documentation

--- CryptoPro.Adapter/CryptCpConfiguration.cs
+++ CryptoPro.Adapter/CryptCpConfiguration.cs
@@ -1,21 +1,21 @@
 ﻿namespace CryptoPro.Adapter.CryptCP
 {
     /// <summary>
-    /// 
+    /// Конфигурация для работы с утилитой CryptCP
     /// </summary>
     public class CryptCpConfiguration
     {
         /// <summary>
-        /// 
+        /// Инициализация экземпляра <see cref="CryptCpConfiguration"/>
         /// </summary>
-        /// <param name="pathExe"></param>
+        /// <param name="pathExe">Путь к исполняемому exe-файлу утилиты</param>
         public CryptCpConfiguration(string pathExe)
         {
             PathExe = pathExe;
         }
 
         /// <summary>
-        /// 
+        /// Расположение исполняемого exe-файла
         /// </summary>
         public string PathExe { get; set; }
     }
